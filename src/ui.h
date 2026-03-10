c9ffb8c971b7d8255e6912aad10f5d6c1a011da0
nkilmer99 <nkilmer@purdue.edu>
2026-03-10 12:05:09 -0400

    Removed excess cpp code

--- src/ui.h
+++ src/ui.h
@@ -9,17 +9,9 @@
 
 #include "pico/async_context_freertos.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 void send_data(uint16_t data);
 void write_lines(char * lines);
 void ui_init();
 void ui_worker(async_context_t *context, async_at_time_worker_t *worker);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif
