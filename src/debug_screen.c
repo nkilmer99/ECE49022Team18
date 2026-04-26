7f9421b4dfc564183eb067a2d2fe6619550331e1
nkilmer99 <nkilmer@purdue.edu>
2026-04-26 13:03:27 -0400

    Debug info

--- src/debug_screen.c
+++ src/debug_screen.c
@@ -15,7 +15,7 @@
 #define AREA_PADDING 0
 #define AREA_DIFF (AREA_HEIGHT + AREA_PADDING)
 
-#define LINE_BUF_SIZE 100
+#define LINE_BUF_SIZE 100 // 100
 
 lv_obj_t * debug_screen = NULL;
 
@@ -56,13 +56,28 @@ uint32_t scroll_pos = 0;
 char * tmp_buf;
 
 void debug_screen_init() {
+  printf("Debug init!\n");
   //Make screen
   debug_screen = lv_obj_create(NULL);
 
   // Alloc stats
+  printf("Alloc stats!\n");
   stats = pvPortMalloc(sizeof(struct line) * STATS_SIZE);
 
+  // Print heap stats
+  struct xHeapStats heapStats;
+  vPortGetHeapStats(&heapStats);
+  printf("Heap stats:\n");
+  printf("xAvailableHeapSpaceInBytes: %zu\n", heapStats.xAvailableHeapSpaceInBytes);
+  printf("xSizeOfLargestFreeBlockInBytes: %zu\n", heapStats.xSizeOfLargestFreeBlockInBytes);
+  printf("xSizeOfSmallestFreeBlockInBytes: %zu\n", heapStats.xSizeOfSmallestFreeBlockInBytes);
+  printf("xNumberOfFreeBlocks: %zu\n", heapStats.xNumberOfFreeBlocks);
+  printf("xMinimumEverFreeBytesRemaining: %zu\n", heapStats.xMinimumEverFreeBytesRemaining);
+  printf("xNumberOfSuccessfulAllocations: %zu\n", heapStats.xNumberOfSuccessfulAllocations);
+  printf("xNumberOfSuccessfulFrees: %zu\n", heapStats.xNumberOfSuccessfulFrees);
+
   // Add stats to screen
+  printf("Setting up stats!\n");
   for (int i = 0; i < STATS_SIZE; i++) {
     lv_obj_t * label = lv_label_create(debug_screen);
     lv_obj_set_x(label, 2);
@@ -85,23 +100,33 @@ void debug_screen_init() {
   lv_style_set_border_width(&small_style, 1);
 
   // Alloc inputs
+  printf("Alloc inputs!\n");
   inputs = pvPortMalloc(sizeof(struct line) * STATS_SIZE);
 
   // Add inputs to screen
+  printf("Setting up inputs!\n");
   for (int i = 0; i < INPUTS_SIZE; i++) {
+    printf("i: %d\n", i);
+    printf("textarea\n");
     lv_obj_t * textarea = lv_textarea_create(debug_screen);
+    printf("set xy\n");
     lv_obj_set_x(textarea, 2);
     lv_obj_set_y(textarea, (STATS_SIZE * LABEL_DIFF) + (i * AREA_DIFF));
+    printf("set placeholder\n");
     lv_textarea_set_placeholder_text(textarea, "Placeholder");
+    printf("set style, size\n");
     lv_obj_add_style(textarea, &small_style, 0);
     lv_obj_set_size(textarea, 124, 18);
 
+    printf("Alloc input buf\n");
     inputs[i].buf = pvPortMalloc(LINE_BUF_SIZE);
+    printf("Set cursor, textarea\n");
     inputs[i].cursor = 0;
     inputs[i].lv_obj = textarea;
   }
 
   // Alloc tmp_buf
+  printf("Alloc tmp_buf!\n");
   tmp_buf = pvPortMalloc(LINE_BUF_SIZE);
 }
 
